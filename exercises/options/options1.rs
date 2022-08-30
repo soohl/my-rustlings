9b758cfe414e6fe29ba539eedda5a68ab57bc921
soohl <soohl@kakao.com>
2022-08-29 23:26:23 -0400

    now

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -10,7 +10,12 @@
 fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22
     // The Option output should gracefully handle cases where time_of_day > 24.
-    ???
+    match time_of_day {
+        0..=21 => Some(5),
+        22..=24 => Some(5),
+        _ => None,
+    }
+    None;
 }
 
 #[cfg(test)]
